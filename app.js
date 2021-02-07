2498be2a17ddb05318f20162a68f4fe8b60583fd
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-02-07 12:34:52 +0300

    Added product delete route

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const products = require("./data");
+let products = require("./data");
 
 const express = require("express");
 
@@ -6,6 +6,19 @@ const app = express();
 
 app.get("/products", (req, res) => {
   res.json({ products });
+});
+
+app.delete("/products/:productId", (req, res) => {
+  const { productId } = req.params;
+  const foundProduct = products.find((product) => product.id === +productId);
+  if (foundProduct) {
+    products = products.filter((product) => product !== foundProduct);
+    res.status(204).end();
+  } else {
+    res
+      .status(404)
+      .json({ message: `A product with id ${productId} does not exist.` });
+  }
 });
 
 app.listen(8000, () => {
