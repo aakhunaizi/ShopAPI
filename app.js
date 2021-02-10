ff8c05559057177bb3b1b3789a0b61d279786745
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-02-10 13:54:21 +0300

    Added error handling and not found middleware

--- app.js
+++ app.js
@@ -6,8 +6,27 @@ const productRoutes = require("./routes/products");
 
 const app = express();
 
+//Middleware
+
 app.use(express.json());
 app.use("/products", productRoutes);
+
+//Not Found Middleware
+
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: "The product is in another castle :(",
+  });
+});
+
+//Error Handling Middleware
+
+app.use((err, req, res, next) => {
+  res
+    .status(err.status ? err.status : 500)
+    .json({ message: err.message ? err.message : "Internal Server Error" });
+});
 
 db.sequelize.sync({ alter: true });
 
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,51 +1,63 @@
 const { Product } = require("../db/models/");
 
-exports.productCreate = async (req, res) => {
+exports.productCreate = async (req, res, next) => {
   try {
     const newProduct = await Product.create(req.body);
     res.status(201).json(newProduct);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
-exports.productList = async (req, res) => {
+exports.productList = async (req, res, next) => {
   try {
     const products = await Product.findAll();
     if (products.length > 0) {
       res.status(200).json(products);
     } else {
-      res.status(404).json({ message: "No Products" });
+      err = {
+        status: 404,
+        messsage: "No Products",
+      };
+      next(err);
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
-exports.productUpdate = async (req, res) => {
+exports.productUpdate = async (req, res, next) => {
   try {
     const foundProduct = await Product.findByPk(req.params.productId);
     if (foundProduct) {
       await foundProduct.update(req.body);
       res.status(204).end();
     } else {
-      res.status(404).json({ message: "Product not found" });
+      err = {
+        status: 404,
+        messsage: "Product not found",
+      };
+      next(err);
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
-exports.productDelete = async (req, res) => {
+exports.productDelete = async (req, res, next) => {
   try {
     const foundProduct = await Product.findByPk(req.params.productId);
     if (foundProduct) {
       await foundProduct.destroy();
       res.status(204).end();
     } else {
-      res.status(404).json({ message: "Product not found" });
+      err = {
+        status: 404,
+        messsage: "Product not found",
+      };
+      next(err);
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
